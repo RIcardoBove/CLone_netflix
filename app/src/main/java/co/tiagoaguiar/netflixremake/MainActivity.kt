e9ff3010a3d24b314dd2fd1b65ddb0f0b3d1cfa1
Ricardo Bove <ricardobove57@gmail.com>
2025-04-08 22:06:25 -0300

    Create first RecyclerView for list movies

--- app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MainActivity.kt
@@ -1,11 +1,41 @@
 package co.tiagoaguiar.netflixremake
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = MainAdapter()
+        val rvMain: RecyclerView = findViewById(R.id.rv_main)
+        rvMain.layoutManager = LinearLayoutManager(this)
+        rvMain.adapter = adapter
+    }
+
+    private inner class MainAdapter: RecyclerView.Adapter<MainAdapter.MovieViewHolder>(){
+
+        private inner class MovieViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+            val view = layoutInflater.inflate(R.layout.movie_item, parent, false)
+            return MovieViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return 60
+        }
+
+        override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+
+        }
+
     }
 }
