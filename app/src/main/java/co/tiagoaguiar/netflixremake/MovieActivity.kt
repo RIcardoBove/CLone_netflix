6030a5f2d3e02031d39418ef02ea678279cde89d
Ricardo Bove <ricardobove57@gmail.com>
2025-04-18 13:30:50 -0300

    Setting the dynamically drawable image

--- app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
@@ -1,8 +1,12 @@
 package co.tiagoaguiar.netflixremake
 
+import android.graphics.drawable.LayerDrawable
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +18,19 @@ class MovieActivity : AppCompatActivity() {
 
         supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_24)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //Busquei o desenhavel (layer-list)
+        val layerDrawable: LayerDrawable = ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
+
+        //Busquei o filme que eu quero
+        val movieCover = ContextCompat.getDrawable(this, R.drawable.movie)
+
+        //atribui o filme para o layer-list
+        layerDrawable.setDrawableByLayerId(R.id.cover_drawable, movieCover)
+
+        //setar a imageView
+        val coverImg: ImageView = findViewById(R.id.movie_img)
+        coverImg.setImageDrawable(layerDrawable)
 
     }
 }
